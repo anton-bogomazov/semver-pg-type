22ea5b2dad4ddc692db096ff320cbca5cda23534
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-07-06 12:54:14 +0200

    add spring application class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "com.abogomazov"
@@ -10,8 +11,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.1")
+    implementation("org.postgresql:postgresql:42.7.3")
+
+    testImplementation("org.testcontainers:postgresql:1.19.8")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
 }
 
 tasks.test {
---
+++ src/main/kotlin/com/abogomazov/Main.kt
@@ -0,0 +1,11 @@
+package com.abogomazov
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SemverExampleApplication
+
+fun main() {
+    runApplication<SemverExampleApplication>()
+}
